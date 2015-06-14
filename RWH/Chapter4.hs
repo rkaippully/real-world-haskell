7c7514cb47d6b696aeed7acb903c72c7b39d2b5a
Raghu Kaippully <rkaippully@gmail.com>
2015-06-14 18:12:09 +0530

    Finished chapter 4

--- RWH/Chapter4.hs
+++ RWH/Chapter4.hs
@@ -8,6 +8,8 @@ License     : GPL Version 3
 module RWH.Chapter4 where
 
 import System.Environment (getArgs)
+import Data.List (foldl')
+import Data.Char
 
 {- |
     Write your own “safe” definitions of the standard partial list functions, but make
@@ -95,3 +97,133 @@ transposeText input output = do
   text <- readFile input
   writeFile output $ unlines $ transpose $ lines text
 
+{- |
+    Use a fold (choosing the appropriate fold will make your code much simpler) to
+    rewrite and improve upon the asInt function from the earlier section“Explicit Recursion”
+    on page 85.
+
+    Your function should behave as follows:
+    ghci> asInt' "101"
+    101
+    ghci> asInt' "-31337"
+    -31337
+    ghci> asInt' "1798"
+    1798
+
+    Extend your function to handle the following kinds of exceptional conditions by
+    calling error:
+    ghci> asInt' ""
+    0
+    ghci> asInt' "-"
+    0
+    ghci> asInt' "-3"
+    -3
+    ghci> asInt' "2.7"
+    *** Exception: Char.digitToInt: not a digit '.'
+    ghci> asInt' "314159265358979323846"
+    564616105916946374
+-}
+asInt' :: String -> Int
+asInt' [] = 0
+asInt' s@(c:cs) =
+  if c == '-'
+  then -1 * foldl' collect 0 cs
+  else foldl' collect 0 s
+  where
+    collect val ch = val*10 + digitToInt ch
+
+{- |
+    The asInt' function uses error, so its callers cannot handle errors. Rewrite
+    the function to fix this problem:
+    -- file: ch04/ch04.exercises.hs
+    type ErrorMessage = String
+    asIntEither :: String -> Either ErrorMessage Int
+    ghci> asIntEither "33"
+    Right 33
+    ghci> asIntEither "foo"
+    Left "non-digit 'o'"
+-}
+type ErrorMessage = String
+
+asIntEither :: String -> Either ErrorMessage Int
+asIntEither [] = Right 0
+asIntEither s@(c:cs) =
+  if c == '-'
+  then
+    let
+      val = foldl' collect (Right 0) cs
+    in
+     case val of
+      err@(Left _) -> err
+      Right v -> Right (-1 * v)
+  else foldl' collect (Right 0) s
+  where
+    collect err@(Left _) _ = err
+    collect (Right val) ch = if isDigit ch
+                             then Right (val*10 + digitToInt ch)
+                             else Left ("non-digit " ++ show ch)
+
+{- |
+    The Prelude function concat concatenates a list of lists into a single list and has the
+    following type:
+    -- file: ch04/ch04.exercises.hs
+    concat :: [[a]] -> [a]
+    Write your own definition of concat using foldr.
+-}
+concat' :: [[a]] -> [a]
+concat' = foldr (++) []
+
+{- |
+    Write your own definition of the standard takeWhile function, first using explicit
+    recursion, and then foldr.
+-}
+takeWhileRecurse :: (a -> Bool) -> [a] -> [a]
+takeWhileRecurse _ [] = []
+takeWhileRecurse f (x:xs) = if f x then x:takeWhileRecurse f xs else []
+
+takeWhileFoldr :: (a -> Bool) -> [a] -> [a]
+takeWhileFoldr f = foldr collect []
+  where
+    collect x acc = if f x then x:acc else []
+
+{- |
+    The Data.List module defines a function, groupBy, which has the following type:
+    -- file: ch04/ch04.exercises.hs
+    groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+    Use ghci to load the Data.List module and figure out what groupBy does, then
+    write your own implementation using a fold.
+-}
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' f = foldr collect []
+  where
+    collect x [] = [[x]]
+    collect x acc@(y:ys)
+      | f x (head y) = (x:y):ys
+      | otherwise = [x]:acc
+
+{- |
+    How many of the following Prelude functions can you rewrite using list folds?
+      . any
+      . cycle
+      . words
+      . unlines
+    For those functions where you can use either foldl' or foldr, which is more appropriate
+    in each case?
+-}
+any' :: (a -> Bool) -> [a] -> Bool
+any' f = foldr (\x y -> y || f x) False  
+
+cycle' :: [a] -> [a]
+cycle' xs = foldr (++) [] (repeat xs)
+
+words' :: String -> [String]
+words' [] = []
+words' str = filter (not . null) $ foldr collect [] str
+  where
+    collect ch [] = [[ch]]
+    collect ch acc@(x:xs)
+      | isSpace ch = []:acc
+      | otherwise = (ch:x):xs
+
+unlines' :: [String] -> String
+unlines' = foldr (\ln acc -> ln ++ "\n" ++ acc) ""
--- RWH/Chapter4Spec.hs
+++ RWH/Chapter4Spec.hs
@@ -6,6 +6,7 @@ module Main where
 
 import Data.Char
 import Test.Hspec
+import Control.Exception
 
 import RWH.Chapter4
 
@@ -50,3 +51,58 @@ main = hspec $ do
       transposeText "test-data/transpose-input.txt" "test-data/transpose-output.txt"
       text <- readFile "test-data/transpose-output.txt"
       text `shouldBe` "hw\neo\nlr\nll\nod\n"
+
+    it "asInt' should handle various cases" $ do
+      asInt' "101" `shouldBe` 101
+      asInt' "-31337" `shouldBe` -31337
+      asInt' "1798" `shouldBe` 1798
+      asInt' "" `shouldBe` 0
+      asInt' "-" `shouldBe` 0
+      asInt' "-3" `shouldBe` -3
+      evaluate (asInt' "2.7") `shouldThrow` errorCall "Char.digitToInt: not a digit '.'"
+      asInt' "314159265358979323846" `shouldBe` 564616105916946374
+
+    it "asIntEither should handle various cases" $ do
+      asIntEither "101" `shouldBe` Right 101
+      asIntEither "-31337" `shouldBe` Right (-31337)
+      asIntEither "1798" `shouldBe` Right 1798
+      asIntEither "" `shouldBe` Right 0
+      asIntEither "-" `shouldBe` Right 0
+      asIntEither "-3" `shouldBe` Right (-3)
+      asIntEither "2.7" `shouldBe` Left "non-digit '.'"
+      asIntEither "314159265358979323846" `shouldBe` Right 564616105916946374
+
+    it "concat' should return empty list for an empty list" $ do
+      concat' ([] :: [String]) `shouldBe` []
+
+    it "concat' should return the concatenated list for other inputs" $ do
+      concat' (["Mary", " had", " a", " little", " lamb"] :: [String]) `shouldBe` "Mary had a little lamb"
+
+    it "takeWhileRecurse on empty list returns empty list" $ do
+      takeWhileRecurse (== 'a') (""::String) `shouldBe` ""
+
+    it "takeWhileRecurse on a non-empty list" $ do
+      takeWhileRecurse (< 10) ([1, 1, 2, 3, 5, 8, 13, 21, 34]::[Int]) `shouldBe` ([1, 1, 2, 3, 5, 8]::[Int])
+
+    it "takeWhileFoldr on empty list returns empty list" $ do
+      takeWhileFoldr (== 'a') (""::String) `shouldBe` ""
+
+    it "takeWhileFoldr on a non-empty list" $ do
+      takeWhileFoldr (< 10) ([1, 1, 2, 3, 5, 8, 13, 21, 34]::[Int]) `shouldBe` ([1, 1, 2, 3, 5, 8]::[Int])
+
+    it "groupBy' groups Mississippi correctly" $ do
+      groupBy' (==) ("Mississippi" :: String) `shouldBe` (["M","i","ss","i","ss","i","pp","i"] :: [String])
+
+    it "any' tests" $ do
+      any' even ([1, 2, 3, 4] :: [Int]) `shouldBe` True
+      any' odd ([2, 4, 6, 8] :: [Int]) `shouldBe` False
+      any' odd ([] :: [Int]) `shouldBe` False
+
+    it "cycle' tests" $ do
+      take 10 (cycle' ([1, 2, 3] :: [Int])) `shouldBe` ([1, 2, 3, 1, 2, 3, 1, 2, 3, 1] :: [Int])
+
+    it "words' tests" $ do
+      words' "Mary  had \t a little lamb" `shouldBe` ["Mary", "had", "a", "little", "lamb"]
+
+    it "unlines' tests" $ do
+      unlines' ["Twinkle", "twinkle", "little", "star"] `shouldBe` "Twinkle\ntwinkle\nlittle\nstar\n"
